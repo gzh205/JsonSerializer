a46d9ee497425dd0df9a9c36e7948504c9e5edea
gzh205 <gzh205@github.com>
2021-04-07 13:32:16 +0800

    create new files

---
+++ JsonSerializer/Json.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+namespace JsonSerializer {
+    public class Json {
+        public static Object DeSerialize<T>(String Json) {
+            Type type = typeof(T);
+            T t = (T)Activator.CreateInstance(type);
+            PropertyInfo[] properties = type.GetProperties();
+            Deserializer de = new Deserializer();
+            de.getObject(t,Json);
+            return t;
+        }
+        public static String Serializer(Object obj) {
+            Serializer se = new Serializer();
+            return se.getObject(obj);
+        }
+    }
+}
